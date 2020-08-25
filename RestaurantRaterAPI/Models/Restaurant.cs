225c5130b630d70ca61547ca551d6c68f1d1fc03
Chris Blinde <chrisblinde@gmail.com>
2020-08-25 10:39:03 -0400

    Added Restaurant Entity

---
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+
+        //public bool  IsRecomended => Rating > 3.5;
+        public bool IsRecomended
+        {
+            get
+            {
+                return Rating > 3.5;
+            }
+        }
+    }
+}
+                            
+        
